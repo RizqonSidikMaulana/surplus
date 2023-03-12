aa1b7127bcd6e0c2fca0a195dcedd41d2b3fe20f
RizqonSidikMaulana <Rizqon.sidik.maulana@gmail.com>
2023-03-12 15:50:15 +0700

    create migration, seed, model, controller for images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ProductSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ImageSeeder::class);
     }
 }
